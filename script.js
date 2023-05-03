a4d4c00189896c6dfc5d6b755f3be56b206bb7a7
carlos andre <carlosandregomes8@gmail.com>
2023-05-03 19:18:56 -0300

    commit do javascript
    projeto ok

---
+++ script.js
@@ -0,0 +1,27 @@
+const outputEl = document.querySelector('.text');
+const valEl = document.querySelectorAll('.val');
+const ClearEl = document.querySelector('.AC');
+const deleteEl = document.querySelector('.DEL');
+const equalEL = document.querySelector('.equal');
+
+valEl.forEach((val)=>{
+  val.addEventListener("click",()=>{
+      outputEl.value += val.value ;
+  })
+})
+
+ClearEl.addEventListener("click", () => {
+  outputEl.value = "";
+})
+
+deleteEl.addEventListener("click", () => {
+  outputEl.value = outputEl.value.slice(0,-1);
+})
+
+equalEL.addEventListener("click", () => {
+  try{
+    outputEl.value = eval(outputEl.value);
+  }catch(e) {
+    alert(e);
+  }
+})
